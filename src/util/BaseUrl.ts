fdc0fed107edcca4ff8ed33d28e722af88701379
Heewon Seo <45059120+a1e0-heewon@users.noreply.github.com>
2022-07-31 22:43:57 +0900

    fix: Update BaseUrl

--- src/util/BaseUrl.ts
+++ src/util/BaseUrl.ts
@@ -1,10 +1,10 @@
 export const USER_LOGIN_API_URL =
-  "http://cloth-service.herokuapp.com/auth/google/login";
+  "https://cloth-service.herokuapp.com/auth/google/login";
 
-export const STORES_API_URL = "http://cloth-service.herokuapp.com/stores";
+export const STORES_API_URL = "https://cloth-service.herokuapp.com/stores";
 export const STORES_DETILE_API_URL =
-  "http://cloth-service.herokuapp.com/stores";
+  "https://cloth-service.herokuapp.com/stores";
 
-export const CLOTHES_API_URL = "http://cloth-service.herokuapp.com/clothes";
+export const CLOTHES_API_URL = "https://cloth-service.herokuapp.com/clothes";
 export const CLOTHES_DETILE_API_URL =
-  "http://cloth-service.herokuapp.com/clothes";
+  "https://cloth-service.herokuapp.com/clothes";
